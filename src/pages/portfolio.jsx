c2c4016ef9e10aacfcea937ab95c87d50ad86f1b
Ugohills1 <ugohils@gmail.com>
2025-06-14 13:55:35 +0100

    resolving portfolio error

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Navbar from "../components/navbar";
-import Content from "../components/content";
 import Footer from "../components/footer";
 import Portfolio from "../components/Portfolio/Portfolio";
+
+
 
 function PortfolioPage() {
     return (
